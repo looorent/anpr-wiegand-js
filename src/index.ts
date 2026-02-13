cf995ef67c2e0850d53a075af1963ab3292c5232
Lorent Lempereur <lorent.lempereur@gmail.com>
2026-02-13 20:51:53 +0100

    Export type Wiegand26Result

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { decode as decode26, encode as encode26 } from "./internal/wiegand26";
+export { decode as decode26, encode as encode26, type Wiegand26Result } from "./internal/wiegand26";
 export { decode as decode64, encode as encode64 } from "./internal/wiegand64";
